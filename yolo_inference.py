0ee860a5a67583e695b103ccffd461f6931c2022
piyushg-07 <guptapiyush315@gmail.com>
2025-01-22 01:41:40 +0530

    first commit

---
+++ yolo_inference.py
@@ -0,0 +1,24 @@
+# from ultralytics import YOLO 
+
+# model = YOLO('yolov8n')
+
+# results = model.predict('input_videos/08fd33_4.mp4',save=True)
+# print(results[0])
+# print('=====================================')
+# for box in results[0].boxes:
+#     print(box)
+    
+    
+    
+    
+    
+    
+from ultralytics import YOLO 
+
+model = YOLO('models/best.pt')
+
+results = model.predict('input_videos/08fd33_4.mp4',save=True)
+print(results[0])
+print('=====================================')
+for box in results[0].boxes:
+    print(box)    
